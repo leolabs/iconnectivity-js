79ef23d3c57df86f5ed270254abd79cda77e1c8f
Leo Bernard <leo@leolabs.org>
2022-07-14 23:55:13 +0200

    :sparkles: Improve the demo page with better error handling in unsupported browsers

--- packages/demo/src/pages/index.tsx
+++ packages/demo/src/pages/index.tsx
@@ -5,21 +5,31 @@ import React, { useEffect, useRef, useState } from "react";
 
 const Component: React.FC = () => {
   const managerRef = useRef<DeviceManager>();
+  const [error, setError] = useState<string>();
   const [devices, setDevices] = useState<Device[]>([]);
   const [deviceInfo, setDeviceInfo] = useState<any>();
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      let unsubscribe: () => void;
-
-      navigator.requestMIDIAccess({ sysex: true }).then((access) => {
-        const manager = new DeviceManager(access);
-        unsubscribe = manager.devicesChanged.addListener((d) => setDevices(d));
-        managerRef.current = manager;
-      });
-
-      return unsubscribe;
+    if (typeof window === "undefined") {
+      return;
     }
+
+    if (!("requestMIDIAccess" in navigator)) {
+      setError(
+        "Your browser doesn't support Web MIDI. Please use Chrome, Opera, or Edge."
+      );
+      return;
+    }
+
+    let unsubscribe: () => void;
+
+    navigator.requestMIDIAccess({ sysex: true }).then((access) => {
+      const manager = new DeviceManager(access);
+      unsubscribe = manager.devicesChanged.addListener((d) => setDevices(d));
+      managerRef.current = manager;
+    });
+
+    return () => unsubscribe?.();
   }, []);
 
   useEffect(() => {
@@ -29,8 +39,8 @@ const Component: React.FC = () => {
 
         return {
           serialNumber: device.serialNumber,
-          supportedCommands: device.getSupportedCommandNames(),
           info,
+          supportedCommands: device.getSupportedCommandNames(),
         };
       })
     ).then(setDeviceInfo);
@@ -39,16 +49,30 @@ const Component: React.FC = () => {
   return (
     <div>
       <h2>Devices:</h2>
-      <ul>
-        {devices.map((d) => (
-          <li key={d.serialNumber}>
-            {d.serialNumber} ({Product[d.info.productId]}):{" "}
-            {JSON.stringify(d.info)}
-          </li>
-        ))}
-      </ul>
-
-      <pre>{JSON.stringify(deviceInfo, null, 2)}</pre>
+      {error ? (
+        <p>
+          <b>{error}</b>
+        </p>
+      ) : devices.length ? (
+        <ul>
+          {devices.map((d) => (
+            <li key={d.serialNumber}>
+              <b>{Product[d.info.productId]}</b>:<br />
+              <pre>
+                {JSON.stringify(
+                  deviceInfo.find(
+                    (di: any) => di.serialNumber === d.serialNumber
+                  ),
+                  null,
+                  2
+                )}
+              </pre>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Connect a device to view its details here</p>
+      )}
     </div>
   );
 };
