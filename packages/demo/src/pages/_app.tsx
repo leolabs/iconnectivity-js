e1d0a8d68996f5fc0412f10999033efb18012026
Leo Bernard <leo@leolabs.org>
2022-07-16 00:29:51 +0200

    :bug: Fix trying to assign something to a non-existing window object

--- packages/demo/src/pages/_app.tsx
+++ packages/demo/src/pages/_app.tsx
@@ -2,8 +2,9 @@ import { AppProps } from "next/app";
 import globalStyles from "../styles";
 
 // Make all functions of the library available in the console
-import * as iconnectivity from "iconnectivity-js";
-(window as any).iconnectivity = iconnectivity;
+if (typeof window !== "undefined") {
+  (window as any).iconnectivity = require("iconnectivity-js");
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   globalStyles();
