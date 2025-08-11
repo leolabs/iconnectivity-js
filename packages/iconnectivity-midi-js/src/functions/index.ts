556501b878be6ce2c9fc55a5578f433f9be000f8
Leo Bernard <leo@leolabs.org>
2025-08-11 20:40:41 +0200

    :sparkles: Add a function to set the failover arm state

--- packages/iconnectivity-midi-js/src/functions/index.ts
+++ packages/iconnectivity-midi-js/src/functions/index.ts
@@ -3,4 +3,5 @@ export * from "./get-device-name";
 export * from "./get-failover-info";
 export * from "./get-meter-values";
 export * from "./set-alarm-status";
+export * from "./set-failover-armed";
 export * from "./set-scene";
